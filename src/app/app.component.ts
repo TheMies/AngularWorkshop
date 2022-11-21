62861325151fe29f235f832a278080936708ee6d
Michel van Geel <michel.van.geel@daqs.io>
2022-11-21 16:22:28 +0100

    Workshop 08 Bind to an object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    money:number = 100; // backing field
-
-  get Money(): number{
-    return this.money;
-  }
-
-  set Money(value: number){
-    if (value > 1000){
-      alert("Value must be <= 1000");
-    }
-    else{
-      this.money = value;
-    }
-  }
+  user = {name:"Harry", age:42, length:1.86}
 }
-
-
-
-
