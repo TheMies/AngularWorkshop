058421482f200bb3d44ebd540ed1c8eb64b879e0
Michel van Geel <michel.van.geel@daqs.io>
2022-11-21 15:27:06 +0100

    Workshop 06 Two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isUnchanged: boolean = true;
-
-  onToggleEnableState(){
-    this.isUnchanged = !this.isUnchanged;
-  }
+  name: string = "Harry";
 }
 
 
