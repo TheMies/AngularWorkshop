57a29739099cc532204d8b8d27b8f002044c7700
Michel van Geel <michel.van.geel@daqs.io>
2022-11-23 17:16:36 +0100

    Workshop 15 Lifecycle hooks

--- src/app/test/User.ts
+++ src/app/test/User.ts
@@ -6,39 +6,22 @@ interface IUser {
   showNameAgeAndHeight(): string;
 }
 
-export class User {
+export class User implements IUser{
   name: string;
   age: number;
+  height: number;
   
-  constructor(name: string, age: number) {
+  constructor(name: string, age: number, height: number) {
     console.log("In conctructor of User")
     this.name = name;
     this.age = age;
+    this.height = height;
+  }
+  showNameAgeAndHeight(): string {
+    return `${this.name} is ${this.age} years old and is ${this.height} meters tall.`
   }
 
   sayHi(){
     return this.name + " says 'Hi!'."
   }
 }
-
-
-
-// export class User implements IUser{
-//   name: string;
-//   age: number;
-//   height: number;
-  
-//   constructor(name: string, age: number, height: number) {
-//     console.log("In conctructor of User")
-//     this.name = name;
-//     this.age = age;
-//     this.height = height;
-//   }
-//   showNameAgeAndHeight(): string {
-//     return `${this.name} is ${this.age} years old and is ${this.height} meters tall.`
-//   }
-
-//   sayHi(){
-//     return this.name + " says 'Hi!'."
-//   }
-// }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { User } from './User';
 
 @Component({
@@ -6,7 +6,20 @@ import { User } from './User';
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-export class TestComponent {
-    user: User = new User("Herman", 42);
-}
+
+export class TestComponent implements OnInit, OnChanges, OnDestroy {
+    user: User | undefined;
+
+    ngOnInit(): void {
+      this.user = new User("Herman", 42, 1.42);
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+      console.log("Handle changes...");
+    }
+
+    ngOnDestroy(): void {
+      console.log("Cleanup...");
+    }
+  }
 
