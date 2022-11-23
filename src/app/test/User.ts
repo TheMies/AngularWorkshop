dbd3270b8fea827df4488024622c19eea70ce1d4
Michel van Geel <michel.van.geel@daqs.io>
2022-11-23 16:22:36 +0100

    13 Class and Constructor

--- src/app/test/User.ts
+++ src/app/test/User.ts
@@ -1,8 +1,14 @@
-
 export class User {
   name: string;
-  constructor(name: string) {
+  age: number;
+  
+  constructor(name: string, age: number) {
     console.log("In conctructor of User")
     this.name = name;
+    this.age = age;
+  }
+
+  sayHi(){
+    return this.name + " says 'Hi!'."
   }
 }
