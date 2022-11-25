0313b8b8ca346fdfacb63ac068f6af787f536475
Michel van Geel <michel.van.geel@daqs.io>
2022-11-25 10:40:31 +0100

    Added simple test component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'TestComponent';
+  totalValue: string = "";
+
+  onGetTotal(data: string):void{
+    this.totalValue = data;
+  }
+}
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,17 +1,46 @@
-import { Component } from '@angular/core';
-import { LogService } from '../log.service';
-import { User } from './User';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-test',
+  selector: 'daqs-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
+export class TestComponent implements OnInit {
+  title: string = "Button press tester";
+  _multiplier: number = 1;
+  buttonClickCounter: number = 0;
+  @Input() initalMultiplier: number = 0;
+  @Output() onGetTotal: EventEmitter<string> = new EventEmitter<string>();
 
-export class TestComponent {
-  constructor(private log: LogService){
-    log.log("In constructor!");
+  constructor() { }
+
+  ngOnInit(): void {
+    this.multiplier = this.initalMultiplier;
+  }
+
+  onButtonClicked(): void{
+    this.buttonClickCounter++;
+  }
+
+  onGetTotalClicked():void{
+    this.onGetTotal.emit("the total is " + this.total);
+  }
+
+  onResetButtonClicked():void{
+    this.buttonClickCounter = 0;
+  }
+
+
+  set multiplier(value: number) {
+    console.log(value);
+    this._multiplier= value;
+  }
+
+  get multiplier(): number{
+    return this._multiplier;
+  }
+
+  get total(): number{
+    return this._multiplier * this.buttonClickCounter;
   }
 }
-  
-
